eb9c71d430746e5814451e06b48b4e8d81979edb
jsthtlf <justdoorigami@gmail.com>
2023-05-19 14:15:07 +0500

    Структура config.yml

---
+++ config/config.go
@@ -0,0 +1,159 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	prefixName = "[PAM]-"
+
+	hostEnvKey = "SERVER_HOSTNAME"
+
+	defaultNameMaxLen = 128
+)
+
+var GlobalConfig *Config
+
+type Config struct {
+	Name           string `mapstructure:"TERMINAL_NAME"`
+	CoreHost       string `mapstructure:"CORE_HOST"`
+	BootstrapToken string `mapstructure:"BOOTSTRAP_TOKEN"`
+	LogLevel       string `mapstructure:"LOG_LEVEL"`
+	LogFormat      string `mapstructure:"LOG_FORMAT"`
+	LanguageCode   string `mapstructure:"LANGUAGE_CODE"`
+
+	RootPath          string
+	DataFolderPath    string
+	LogDirPath        string
+	KeyFolderPath     string
+	AccessKeyFilePath string
+	ReplayFolderPath  string
+}
+
+func SetupConfig(configPath string) {
+	var conf = getDefaultConfig()
+	loadConfigFromEnv(&conf)
+	loadConfigFromFile(configPath, &conf)
+	GlobalConfig = &conf
+}
+
+func getDefaultConfig() Config {
+	defaultName := getDefaultName()
+
+	rootPath := getPwdDirPath()
+	dataFolderPath := filepath.Join(rootPath, "data")
+	replayFolderPath := filepath.Join(dataFolderPath, "replays")
+	LogDirPath := filepath.Join(dataFolderPath, "logs")
+	keyFolderPath := filepath.Join(dataFolderPath, "keys")
+	accessKeyFilePath := filepath.Join(keyFolderPath, ".access_key")
+
+	folders := []string{dataFolderPath, replayFolderPath, keyFolderPath, LogDirPath}
+	for i := range folders {
+		if err := EnsureDirExist(folders[i]); err != nil {
+			log.Fatalf("Create folder failed: %s", err)
+		}
+	}
+	return Config{
+		Name:           defaultName,
+		CoreHost:       "localhost",
+		BootstrapToken: "",
+		LogLevel:       "INFO",
+		LogFormat:      "%time% [%lvl%] %msg%",
+		LanguageCode:   "ru",
+
+		AccessKeyFilePath: accessKeyFilePath,
+		RootPath:          rootPath,
+		DataFolderPath:    dataFolderPath,
+		LogDirPath:        LogDirPath,
+		KeyFolderPath:     keyFolderPath,
+		ReplayFolderPath:  replayFolderPath,
+	}
+}
+
+func EnsureDirExist(path string) error {
+	if !haveDir(path) {
+		if err := os.MkdirAll(path, os.ModePerm); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func have(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+func haveDir(file string) bool {
+	fi, err := os.Stat(file)
+	return err == nil && fi.IsDir()
+}
+
+func getPwdDirPath() string {
+	if rootPath, err := os.Getwd(); err == nil {
+		return rootPath
+	}
+	return ""
+}
+
+func loadConfigFromEnv(conf *Config) {
+	viper.AutomaticEnv()
+	envViper := viper.New()
+	for _, item := range os.Environ() {
+		envItem := strings.SplitN(item, "=", 2)
+		if len(envItem) == 2 {
+			envViper.Set(envItem[0], viper.Get(envItem[0]))
+		}
+	}
+	if err := envViper.Unmarshal(conf); err == nil {
+		log.Println("Load config from env success")
+	}
+}
+
+func loadConfigFromFile(path string, conf *Config) {
+	var err error
+	if have(path) {
+		fileViper := viper.New()
+		fileViper.SetConfigFile(path)
+		if err = fileViper.ReadInConfig(); err == nil {
+			if err = fileViper.Unmarshal(conf); err == nil {
+				log.Printf("Load config from %s success\n", path)
+				return
+			}
+		}
+	}
+	if err != nil {
+		log.Fatalf("Load config from %s failed: %s\n", path, err)
+	}
+}
+
+/*
+SERVER_HOSTNAME: Имя переменной окружения, может использоваться для настройки префикса зарегистрированного имени по умолчанию
+default name rule:
+[PAM]-{SERVER_HOSTNAME}-{HOSTNAME}
+
+	or
+
+[PAM]-{HOSTNAME}
+*/
+func getDefaultName() string {
+	hostname, _ := os.Hostname()
+	if serverHostname, ok := os.LookupEnv(hostEnvKey); ok {
+		hostname = fmt.Sprintf("%s-%s", serverHostname, hostname)
+	}
+	hostRune := []rune(prefixName + hostname)
+	if len(hostRune) <= defaultNameMaxLen {
+		return string(hostRune)
+	}
+	name := make([]rune, defaultNameMaxLen)
+	index := defaultNameMaxLen / 2
+	copy(name[:index], hostRune[:index])
+	start := len(hostRune) - index
+	copy(name[index:], hostRune[start:])
+	return string(name)
+}

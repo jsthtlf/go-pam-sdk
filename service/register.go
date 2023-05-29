8a276c0a26a6b52b9f724fe08ab994fcc49e5771
jsthtlf <justdoorigami@gmail.com>
2023-05-29 10:00:47 +0500

    service: обновлен функционал
    Добавлен функционал автоматической регистрации и запуска терминала

--- service/register.go
+++ service/register.go
@@ -4,12 +4,80 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/jsthtlf/go-pam-sdk/config"
 	"github.com/jsthtlf/go-pam-sdk/httplib"
+	"github.com/jsthtlf/go-pam-sdk/logger"
 	"github.com/jsthtlf/go-pam-sdk/model"
-	//"github.com/davecgh/go-spew/spew"
 )
+
+func MustPAMService() *PAMService {
+	key := MustLoadValidAccessKey()
+	pamService, err := NewAuthPAMService(PAMCoreHost(
+		config.GetCurrentConfig().CoreHost), PAMTimeOut(30*time.Second),
+		PAMAccessKey(key.ID, key.Secret),
+	)
+	if err != nil {
+		logger.Fatal("Error while creating terminal: %s" + err.Error())
+		os.Exit(1)
+	}
+	return pamService
+}
+
+func MustLoadValidAccessKey() model.AccessKey {
+	conf := config.GetCurrentConfig()
+	var key model.AccessKey
+	if err := key.LoadFromFile(conf.AccessKeyFilePath); err != nil {
+		return MustRegisterTerminalAccount()
+	}
+	// accessKey
+	return MustValidKey(key)
+}
+
+func MustRegisterTerminalAccount() (key model.AccessKey) {
+	conf := config.GetCurrentConfig()
+	for i := 0; i < 10; i++ {
+		terminal, err := RegisterTerminalAccount(conf.CoreHost,
+			conf.Name, conf.BootstrapToken, conf.Comment, conf.TerminalType)
+		if err != nil {
+			logger.Error(err.Error())
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		key.ID = terminal.ServiceAccount.AccessKey.ID
+		key.Secret = terminal.ServiceAccount.AccessKey.Secret
+		if err := key.SaveToFile(conf.AccessKeyFilePath); err != nil {
+			logger.Error("Error while save access key: " + err.Error())
+		}
+		return key
+	}
+	logger.Error("Error while registration terminal")
+	os.Exit(1)
+	return
+}
+
+func MustValidKey(key model.AccessKey) model.AccessKey {
+	conf := config.GetCurrentConfig()
+	for i := 0; i < 10; i++ {
+		if err := ValidAccessKey(conf.CoreHost, key); err != nil {
+			switch {
+			case errors.Is(err, ErrUnauthorized):
+				logger.Error("Access key unauthorized, try to register new access key")
+				return MustRegisterTerminalAccount()
+			default:
+				logger.Error("Check access key failed: " + err.Error())
+			}
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		return key
+	}
+	logger.Error("Error while checking access key")
+	os.Exit(1)
+	return key
+}
 
 func RegisterTerminalAccount(coreHost, token, name, comment, typeTerminal string) (res model.Terminal, err error) {
 	client, err := httplib.NewClient(coreHost, time.Second*30)

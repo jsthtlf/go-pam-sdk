69ad43c71798f8e980eb18ede9c1252c2c3efb37
jsthtlf <justdoorigami@gmail.com>
2024-04-02 12:52:51 +0500

    Обновлена структура конфигурации терминала

--- pkg/model/terminal.go
+++ pkg/model/terminal.go
@@ -1,23 +1,23 @@
 package model
 
 type TerminalConfig struct {
-	AssetListPageSize   string                 `json:"TERMINAL_ASSET_LIST_PAGE_SIZE"`
-	AssetListSortBy     string                 `json:"TERMINAL_ASSET_LIST_SORT_BY"`
-	HeaderTitle         string                 `json:"TERMINAL_HEADER_TITLE"`
-	PasswordAuth        bool                   `json:"TERMINAL_PASSWORD_AUTH"`
-	PublicKeyAuth       bool                   `json:"TERMINAL_PUBLIC_KEY_AUTH"`
-	ReplayStorage       ReplayConfig           `json:"TERMINAL_REPLAY_STORAGE"`
-	CommandStorage      map[string]interface{} `json:"TERMINAL_COMMAND_STORAGE"`
-	SessionKeepDuration int                    `json:"TERMINAL_SESSION_KEEP_DURATION"`
-	TelnetRegex         string                 `json:"TERMINAL_TELNET_REGEX"`
-	MaxIdleTime         int                    `json:"SECURITY_MAX_IDLE_TIME"`
-	HeartbeatDuration   int                    `json:"TERMINAL_HEARTBEAT_INTERVAL"`
-	HostKey             string                 `json:"TERMINAL_HOST_KEY"`
-	EnableSessionShare  bool                   `json:"SECURITY_SESSION_SHARE"`
-	EnableDbWeb         bool                   `json:"TERMINAL_SSH_DB_ENABLED"`
-	EnableRdpWeb        bool                   `json:"TERMINAL_RDP_ENABLED"`
-	EnableRdpNative     bool                   `json:"TERMINAL_RDP_NATIVE_ENABLED"`
-	EnableDbNative      bool                   `json:"TERMINAL_DB_NATIVE_ENABLED"`
+	AssetListPageSize   string        `json:"TERMINAL_ASSET_LIST_PAGE_SIZE"`
+	AssetListSortBy     string        `json:"TERMINAL_ASSET_LIST_SORT_BY"`
+	HeaderTitle         string        `json:"TERMINAL_HEADER_TITLE"`
+	PasswordAuth        bool          `json:"TERMINAL_PASSWORD_AUTH"`
+	PublicKeyAuth       bool          `json:"TERMINAL_PUBLIC_KEY_AUTH"`
+	ReplayStorage       ReplayConfig  `json:"TERMINAL_REPLAY_STORAGE"`
+	CommandStorage      CommandConfig `json:"TERMINAL_COMMAND_STORAGE"`
+	SessionKeepDuration int           `json:"TERMINAL_SESSION_KEEP_DURATION"`
+	TelnetRegex         string        `json:"TERMINAL_TELNET_REGEX"`
+	MaxIdleTime         int           `json:"SECURITY_MAX_IDLE_TIME"`
+	HeartbeatDuration   int           `json:"TERMINAL_HEARTBEAT_INTERVAL"`
+	HostKey             string        `json:"TERMINAL_HOST_KEY"`
+	EnableSessionShare  bool          `json:"SECURITY_SESSION_SHARE"`
+	EnableDbWeb         bool          `json:"TERMINAL_SSH_DB_ENABLED"`
+	EnableRdpWeb        bool          `json:"TERMINAL_RDP_ENABLED"`
+	EnableRdpNative     bool          `json:"TERMINAL_RDP_NATIVE_ENABLED"`
+	EnableDbNative      bool          `json:"TERMINAL_DB_NATIVE_ENABLED"`
 }
 
 type Terminal struct {
@@ -70,4 +70,19 @@ type ReplayConfig struct {
 	AccountKey     string `json:"ACCOUNT_KEY,omitempty"`
 	EndpointSuffix string `json:"ENDPOINT_SUFFIX,omitempty"`
 	ContainerName  string `json:"CONTAINER_NAME,omitempty"`
+}
+
+type CommandConfig struct {
+	TypeName string `json:"TYPE"`
+
+	/*
+		elasticsearch
+	*/
+	Hosts       []string `json:"HOSTS,omitempty"`
+	IndexByDate bool     `json:"INDEX_BY_DATE,omitempty"`
+	Index       string   `json:"INDEX,omitempty"`
+	DocType     string   `json:"DOC_TYPE,omitempty"`
+	Other       struct {
+		IgnoreVerifyCerts bool `json:"IGNORE_VERIFY_CERTS,omitempty"`
+	} `json:"OTHER"`
 }
--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -35,25 +35,13 @@ type ReplayStorage interface {
 }
 
 func NewCommandStorage(pamService *service.PAMService, conf *model.TerminalConfig) CommandStorage {
-	cf := conf.CommandStorage
-	tp, ok := cf["TYPE"]
-	if !ok {
-		tp = TypeServer
-	}
-	switch tp {
+	cfg := conf.CommandStorage
+	switch cfg.TypeName {
 	case TypeES, TypeElasticSearch:
-		var hosts = make([]string, len(cf["HOSTS"].([]interface{})))
-		for i, item := range cf["HOSTS"].([]interface{}) {
-			hosts[i] = item.(string)
-		}
-		var skipVerify bool
-		index := cf["INDEX"].(string)
-		docType := cf["DOC_TYPE"].(string)
-		if otherMap, ok := cf["OTHER"].(map[string]interface{}); ok {
-			if insecureSkipVerify, ok := otherMap["IGNORE_VERIFY_CERTS"]; ok {
-				skipVerify = insecureSkipVerify.(bool)
-			}
-		}
+		hosts := cfg.Hosts
+		index := cfg.Index
+		docType := cfg.DocType
+		skipVerify := cfg.Other.IgnoreVerifyCerts
 		if index == "" {
 			index = "pam"
 		}

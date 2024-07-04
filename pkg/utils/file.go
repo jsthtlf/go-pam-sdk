e721a4ef8c53a8b02eb1c0d0cdd370092a88720c
jsthtlf <rbakiev@redsearch.ru>
2024-07-04 09:09:50 +0000

    utils: gzip.go перенесен в file.go

--- pkg/utils/file.go
+++ pkg/utils/file.go
@@ -1,6 +1,12 @@
 package utils
 
-import "os"
+import (
+	"compress/gzip"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+)
 
 func haveDir(file string) bool {
 	fi, err := os.Stat(file)
@@ -23,4 +29,25 @@ func FileExists(name string) bool {
 		}
 	}
 	return true
+}
+
+func CompressToGzipFile(srcPath, dstPath string) error {
+	sf, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	defer sf.Close()
+	df, err := os.Create(dstPath)
+	if err != nil {
+		return err
+	}
+	defer df.Close()
+	writer := gzip.NewWriter(df)
+	writer.Name = filepath.Base(srcPath)
+	writer.ModTime = time.Now().UTC()
+	_, err = io.Copy(writer, sf)
+	if err != nil {
+		return err
+	}
+	return writer.Close()
 }
